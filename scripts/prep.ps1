8ed96af99507af36fa168cf3b61ab09e68d693fc
Pamela Fox <pamela.fox@gmail.com>
2023-05-16 16:40:49 +0000

    Inline postprovision scripts

--- scripts/prep.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host ""
-Write-Host "Copying azd environment variables to .env file"
-Write-Host ""
-
-$output = azd env get-values
-
-Add-Content -Path .env -Value $output
--- scripts/prep.sh
+++
@@ -1,5 +0,0 @@
- #!/bin/sh
-
-echo "Copying azd environment variables to .env file"
-
-azd env get-values > .env
