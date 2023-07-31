5623841f8624162b5022d0ac7c3802bd2c63c117
Pamela Fox <pamela.fox@gmail.com>
2023-07-31 20:48:16 +0000

    Use published library

--- src/flaskapp/static/index.js
+++ src/flaskapp/static/index.js
@@ -18,35 +18,16 @@ form.addEventListener("submit", async function(e) {
     let messageDiv = assistantTemplateClone.querySelector(".message-content");
     targetContainer.appendChild(assistantTemplateClone);
 
-    //for await (const part of stream) {
-    //    console.log(part.choices[0].delta);
-    //}
-
-    let answer = "";
     const response = await fetch(`/chat?message=${message}`);
-    const reader = response.body.getReader();
     messageDiv.innerHTML = "";
-    let runningText = "";
-    
-    while (true) {
-        const { done, value } = await reader.read();
-        if (done) break;
-        var text = new TextDecoder("utf-8").decode(value);
-        const objects = text.split("\n");
-        objects.forEach((obj) => {
-            try {
-                runningText += obj;
-                result = JSON.parse(runningText);
-                if (result["choices"] && result["choices"][0]["delta"]["content"]) {
-                    answer += result["choices"][0]["delta"]["content"];
-                    messageDiv.innerHTML = converter.makeHtml(answer);
-                    messageDiv.scrollIntoView();
-                }
-                runningText = "";
-            }
-            catch { }
-        });
-    };
+    let answer = "";
+    for await (const event of readNDJSONStream(response.body)) {
+        if (event["choices"][0]["delta"]["content"]) {
+            answer += event["choices"][0]["delta"]["content"];
+            messageDiv.innerHTML = converter.makeHtml(answer);
+            messageDiv.scrollIntoView();
+        }
+    }
 
     messageInput.value = "";
 });
